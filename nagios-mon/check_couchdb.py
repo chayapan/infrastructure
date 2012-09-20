1676943e1fb5dd0d57218f69747d16d05d4ab1a5
Chayapan Khannabha <chayapan@gmail.com>
2012-09-20 18:27:06 +0700

    update latest

--- nagios-mon/check_couchdb.py
+++ nagios-mon/check_couchdb.py
@@ -15,26 +15,63 @@
 #   under the License.
 
 """check_couchdb.py  monitors CouchDB with Nagios and NRPE
-Usage: check_couchdb.py -H <host> [-p <port>] [-w <warn_time>]
+Usage: check_couchdb.py -H <host> [-p <port>] [-w <warn_time>] [-U <user:pwd>]
 
   Options:
    -h, --help
       Print detailed help screen
+   -U, --user
+      User account to connect. username:password
    -H, --hostname=ADDRESS
       Host name, IP Address, or unix socket (must be an absolute path)
    -p, --port=INTEGER
-      Port number (default: 389)
+      Port number (default: 5984)
    -a [--active]
       Check active worker count instead of job age
-beanstalkc 0.3.0 (http://pypi.python.org/pypi/beanstalkc/0.3.0)
-        pip install beanstalkc pyyaml
+couchdb (http://pypi.python.org/pypi/couchdb)
+        pip install couchdb
 """
 
 import platform, sys, os
 from optparse import OptionParser
 
 try:
-    import couchdbkit
+    import couchdb
 except Exception:
-    print "Make sure Python has "
+    print """SERVICE STATUS: Missing dependency. Make sure Python has couchdb. Try 'pip install couchdb'"""
+    sys.exit(3)
 
+def couchdb_up(hostname,account=False):
+    #try:
+    server = couchdb.Server(host)
+
+def couchdb_stats():
+	print "stats"
+
+def main():
+	parser = OptionParser() # __doc__
+	parser.add_option('-H', '--hostname',	dest="host",
+		default='localhost', help="Couchdb hostname")
+	parser.add_option('-p', '--port',	dest="port",
+		default='5984', help="Couchdb port")
+	parser.add_option('-v', '--verbose',	dest="verbose",
+		default='0', help="Output details, 0-3")
+	parser.add_option('-c',dest="threshold_critical")
+	parser.add_option('-w',dest="threshold_warning")
+	parser.add_option('-U','--user',	dest="user",
+		default='', help="User account")
+
+	(options, args) = parser.parse_args()
+		
+	host = "http://%(user)s%(host)s:%(port)s" % { 
+		'user' : options.user,
+		'host' : options.host,
+		'port' : options.port
+	}
+	service_status = couchdb_up(host)
+	if (int(options.verbose) > 0 and service_status == 0):
+		couchdb_stats(host,port)
+	return service_status
+
+if (__name__=='__main__'):
+    sys.exit(main())
