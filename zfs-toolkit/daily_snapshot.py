7a2441c9f18a450de83cb32d6c4359a69beb7a04
root <root@pve1.tegan.disqrete.com>
2012-12-09 08:32:43 +0700

    updated logging

--- zfs-toolkit/daily_snapshot.py
+++ zfs-toolkit/daily_snapshot.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
+"""
+  TODO: 
+   - implement hourly snapshot
+   - measure performance for this rotating strategy
+"""
 import os,subprocess,datetime,logging
-logging.basicConfig(	filename='/tmp/snapshot.log',
+logging.basicConfig(	filename='/tank/sysadmin/snapshot.log',
 			level=logging.INFO,
+			format='%(asctime)s %(message)s')
+logging.basicConfig(	filename='/tank/sysadmin/snapshot.error',
+			level=logging.ERROR,
 			format='%(asctime)s %(message)s')
 ZFS = '/sbin/zfs'
 ZPOOL = '/sbin/zpool'
 snaptime = datetime.datetime.now().strftime('%Y%m%d.h%H')
-
-err=open("/tmp/snapshot.err","w")
-err.write(snaptime)
-
+#logging.info("Start snapshot at " + snaptime)
 
 def create_hourly_snapshot():
 	print snaptime
@@ -20,6 +25,9 @@ def create_hourly_snapshot():
 
 def rotate_snapshots(i=4,tgt="tank/vmbak"):
   """ Assume there are i+1 snapshots to be kept
+     1. destroy oldest snapshot
+     2. rename snapshots
+     3. create new snapshot 
   """
   #print i, "%(zfs)s destroy %(tgt)s@last.%(last)s" % {'zfs': ZFS, 'tgt': tgt, 'last': i}
   cmd = "%(zfs)s destroy %(tgt)s@last.%(last)s" % {'zfs': ZFS, 'tgt': tgt, 'last': i}
@@ -30,16 +38,21 @@ def rotate_snapshots(i=4,tgt="tank/vmbak"):
      cmd = "%(zfs)s rename %(tgt)s@last.%(from)s %(tgt)s@last.%(to)s" % {
 			'zfs':ZFS, 'tgt':tgt, 'from':i-1, 'to':i }
      cmd_list.append(cmd)
-  cmd = "%(zfs)s snap %(tgt)s@last.0" % {'zfs': ZFS, 'tgt': tgt}
+  cmd = "%(zfs)s snapshot %(tgt)s@last.0" % {'zfs': ZFS, 'tgt': tgt}
   cmd_list.append(cmd)
   
   #print cmd_list
   for cmd in cmd_list:
     logging.info(cmd)
-    subprocess.call(cmd.split(" "),stderr=err)
+    subprocess.call(cmd.split(" "))
 
 if __name__ == '__main__':
+  """
+    Assume 4 datasets: tank/vm tank/sysadmin tank/othello tank/dune
+  """
+  logging.info("Start snapshot at " + snaptime)
   rotate_snapshots(tgt="tank/vm")
-  rotate_snapshots(tgt="tank/vmbak")
-
-err.close()
+  rotate_snapshots(tgt="tank/sysadmin")
+  rotate_snapshots(tgt="tank/othello")
+  rotate_snapshots(tgt="tank/dune")
+  logging.info("End snapshot at " + snaptime)
